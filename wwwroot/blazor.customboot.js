1dd4e818823c7b71fd3a89e2edd60d8be33b1f39
jsakamoto <junichi.jp@gmail.com>
2021-12-21 21:37:38 +0900

    Configure the Blazor Server to keep reconnecting indefinitely.

---
+++ wwwroot/blazor.customboot.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+
+Blazor.start({
+    // Keep retrying each 2 seconds for a year (!).
+    // (This is, in effect, an infinite number of retries.)
+    reconnectionOptions: { maxRetries: 15768000, retryIntervalMilliseconds: 2000 },
+});
